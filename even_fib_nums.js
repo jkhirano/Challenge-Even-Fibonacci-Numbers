3ea2a84f6895e9209be7ee151d4a40561d5578aa
Janelle K Hirano <jaahnelle@gmail.com>
2019-07-06 10:33:19 -1000

    finished

--- even_fib_nums.js
+++ even_fib_nums.js
@@ -5,31 +5,44 @@
  * @param  {Number} maxFibValue
  * @return {Number} sum
  */
-function _sumFibs( maxFibValue ) {
-  let sum = 0;
+function _sumFibs(maxFibValue) {
+  debugger;
+  let sum = 2;
 
   // do your work here
 
+  let first = 1;
+  let second = 2;
+  let current = 3;
+  for (let i = 2; current <= maxFibValue; i++) {
+    current = first + second;
+    first = second;
+    second = current;
+
+    if (current % 2 === 0) {
+      sum += current;
+    }
+  }
+  console.log(sum);
   return sum;
 }
 
 // bonus round
-function _highestFibonacciNumber (maxFibValue){
+function _highestFibonacciNumber(maxFibValue) {
   let highest = 0;
 
   //define your base case, validate your input
 
-
   //do your work here
 
   return highest;
-};
+}
 
 /**
  * Do not modify code below.
  * You must be at least level 10 to understand.
  */
 module.exports = {
-  sumFibs : _sumFibs,
-  highestFibonacciNumber : _highestFibonacciNumber
+  sumFibs: _sumFibs,
+  highestFibonacciNumber: _highestFibonacciNumber
 };
